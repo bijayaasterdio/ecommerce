c0e2c855c609406932f0b8d3fefc40ba156c042e
bijayaasterdio <bijay@asterdio.com>
2020-12-10 09:40:54 +0545

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import './css/bootstrap.min.css';
+import './css/style.css';
+import logo from './img/logo.png';
+import baricon from './img/svg/menu.svg';
+import shopicon from './img/svg/shopping-cart.svg'
+import caticon1 from './img/svg/f-icon.svg'
+import banner from './img/banner.svg'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function BookList() {
+  return (
+    <div>
+        <Navi/>
+        <SearchB />
+        <section>
+          <div className="container">
+          <img src={banner} alt=""/>
+          </div>
+        </section>
+        <section>
+          <div className="container">
+            {CatArr.map((category)=>{
+              const {img, text} = category;
+              return (
+                <Category category={category}></Category>
+              );
+            })}
+          </div>
+        </section>
+
+      </div>
+  );
+  
+}
+
+const Navi = () => {
+  return(
+    <section className="listing">
+      <div className="container">
+        <div className="row">
+        <div className="logo col-md-3">
+          <img src={baricon} alt=""/>
+        </div>
+         <div className="col-md-6">
+           <img src={logo} alt=""/>
+         </div>
+         <div className="col-md-3">
+           <img src={shopicon} alt=""/>
+         </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+
+const CatArr = [
+  {
+    img:{caticon1},
+    text:'All Items',
+  },
+  {
+    img:'img/logo.png',
+    text:'Jeweleries',
+  }
+];
+const Category = (props) => {
+  const {img,text} = props.category;
+  return(
+    <div className="block">
+      <div className="icon"><img src={img} alt=""/></div>
+      <div className="text">{text}</div>
+    </div>
+  )
+}
+
+const SearchB = () => {
+  return (
+<div className="searchbox">
+  <div className="container">
+    <input type="text" className="form-control"/>
+    </div>
+    </div>
+  )
+}
+ReactDOM.render(<BookList />, document.getElementById('root'));
